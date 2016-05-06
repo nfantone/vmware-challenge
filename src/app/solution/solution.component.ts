6ea900a3d584a428358af6f5c55f1922358d2ebf
Nicolás Fantone <nfantone@gmail.com>
2016-05-06 02:30:20 -0600

    Fix broken merge

--- src/app/solution/solution.component.ts
+++ src/app/solution/solution.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ContainerService} from '../containers/container.service';
 import {Container} from '../containers/container';
-<<<<<<< HEAD
 import {Observable} from 'rxJS/Observable';
 import {ContainersListComponent} from './containers-list.component';
 
@@ -16,34 +15,11 @@ export class SolutionComponent implements OnInit {
 
   containers: Observable<Container[]>;
   memoryThreshold: number = 0;
-=======
-import {MemoryFormatPipe} from '../util/memoryFormat.pipe';
-import {SortByIdPipe} from './sort-by-id.pipe';
-import {MemoryAllocationPipe} from './memory-allocation.pipe';
-import {Observable} from 'rxJS/Observable';
-
-@Component({
-  selector: 'my-solution',
-  templateUrl: 'app/solution/solution.component.html',
-  styleUrls: ['app/solution/solution.css'],
-  providers: [
-    ContainerService
-  ],
-  pipes: [MemoryFormatPipe, SortByIdPipe, MemoryAllocationPipe]
-})
-export class SolutionComponent implements OnInit {
-  private static CRITITAL_MEMORY_THRESHOLD : number = 80;
-
-  memoryThreshold: number = 0;
-  order: boolean;
-  containers: Observable<Container[]>;
->>>>>>> df066835fe636b417831433df4ab91114725c419
 
   constructor(private containerService: ContainerService) { }
 
   ngOnInit() {
     this.containers = this.containerService.getContainers();
-<<<<<<< HEAD
   }
 
   toggleCriticalFilter(evt: Event) {
@@ -52,24 +28,6 @@ export class SolutionComponent implements OnInit {
 
   toggleContainerState(evt : Event) : Observable<Container> {
     let c = evt.container;
-=======
-  }
-
-  toggleCriticalFilter(evt: Event) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    let elem: HTMLInputElement = <HTMLInputElement>evt.srcElement;
-    if (elem.checked) {
-      this.memoryThreshold = SolutionComponent.CRITITAL_MEMORY_THRESHOLD;
-    } else {
-      this.memoryThreshold = 0;
-    }
-  }
-
-  toggleContainerState(evt : Event, c: Container) : Observable<Container> {
-    evt.preventDefault();
-    evt.stopPropagation();
->>>>>>> df066835fe636b417831433df4ab91114725c419
     return c.state === 'STARTED' ? this.stopContainer(c) : this.startContainer(c);
   }
 
