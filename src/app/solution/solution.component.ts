3c6c65abde1abc7c5bc3ced73d8b2ce0a214692b
Nicolás Fantone <nfantone@gmail.com>
2016-05-05 20:31:38 -0600

    Allow stopping and starting container

--- src/app/solution/solution.component.ts
+++ src/app/solution/solution.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ContainerService} from '../containers/container.service';
 import {Container} from '../containers/container';
-import {MemoryFormatPipe} from '../util/memoryFormat.pipe'
+import {MemoryFormatPipe} from '../util/memoryFormat.pipe';
+import {Observable} from 'rxJS/Observable';
 
 const DEFAULT_CONTAINER_ID = 1;
 
@@ -15,10 +16,18 @@ const DEFAULT_CONTAINER_ID = 1;
 })
 export class SolutionComponent implements OnInit {
   container: Container;
-  constructor(private containerService: ContainerService) {}
+  constructor(private containerService: ContainerService) { }
 
   ngOnInit() {
     this.containerService.getContainer(DEFAULT_CONTAINER_ID)
       .subscribe((c: Container) => this.container = c);
+  }
+
+  stopContainer(evt : Event) : Observable<Container> {
+    return this.containerService.stopContainer(this.container);
+  }
+
+  startContainer(evt: Event) : Observable<Container> {
+    return this.containerService.startContainer(this.container);
   }
 }
